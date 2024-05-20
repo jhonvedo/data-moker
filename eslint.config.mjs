78c49cb634df924557417948f305f3bc1031ae52
Jhon Alexander Acevedo <jhon.acevedo@inxeption.com>
2024-05-20 15:06:45 -0500

    init commit

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+// eslint.config.mjs
+
+import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+    {
+        files: ['**/*.ts'],
+        ignores: ['node_modules'],
+        languageOptions: {
+            parser: typescriptParser,
+            parserOptions: {
+                ecmaVersion: 2021,
+                sourceType: 'module',
+            },
+        },
+        plugins: {
+            '@typescript-eslint': typescriptPlugin,
+            prettier: prettierPlugin,
+        },
+        rules: {
+            ...typescriptPlugin.configs.recommended.rules,
+            ...prettierConfig.rules,
+            'prettier/prettier': 'error',
+        },
+    },
+    {
+        files: ['*.js', '*.mjs'],
+        languageOptions: {
+            ecmaVersion: 2021,
+            sourceType: 'module',
+        },
+        plugins: {
+            prettier: prettierPlugin,
+        },
+        rules: {
+            ...prettierConfig.rules,
+            'prettier/prettier': 'error',
+        },
+    },
+];
